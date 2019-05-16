8cc281880ab95c53b15db695ef34c32b123b0521
BMDroid <ametrojan@gmail.com>
2019-05-16 22:55:01 +0800

    test set uploaded

---
+++ src/createTest.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import time
+
+
+def create_samples(folderName):
+    for fileName in os.listdir(folderName):
+        os.system(f"opencv_createsamples -img {folderName}/{fileName} -bg neg.txt -info tests/test_{fileName[-6:-4]}.txt -pngoutput tests -num 7 -maxxangle 0 -maxyangle 0 -maxzangle 0 -bgcolor 255 -bgthresh 8 -maxdev 40 -w 48 -h 30")
+        time.sleep(1) 
+
+if __name__ == '__main__':
+    folderName = 'pos_resize'
+    create_samples(folderName)
